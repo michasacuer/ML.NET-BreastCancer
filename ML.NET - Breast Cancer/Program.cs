a15650f89c3ca786132a14089c772f7ba9c85b36
Michał Szewczak <michalszewczak2@gmail.com>
2018-09-13 21:13:01 +0200

    Add project files.

---
+++ ML.NET - Breast Cancer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET___Breast_Cancer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
