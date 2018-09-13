9ba97351ae221654bddf334c9db42990e39fc519
Michał Szewczak <michalszewczak2@gmail.com>
2018-09-13 21:13:13 +0200

    Init

--- ML.NET - Breast Cancer/Program.cs
+++ ML.NET - Breast Cancer/Program.cs
@@ -6,7 +6,7 @@ namespace ML.NET___Breast_Cancer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//
         }
     }
 }
