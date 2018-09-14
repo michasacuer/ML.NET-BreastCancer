749557223a985e1a39b50cd0801a17d701e80168
Michał Szewczak <michalszewczak2@gmail.com>
2018-09-14 23:10:07 +0200

    Working example

---
+++ ML.NET - Breast Cancer/CancerPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Runtime.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ML.NET___Breast_Cancer
+{
+    class CancerPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string Diagnosis;
+    }
+}
--- ML.NET - Breast Cancer/Program.cs
+++ ML.NET - Breast Cancer/Program.cs
@@ -1,16 +1,78 @@
 ﻿using System;
 using Accord.DataSets;
 using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Models;
 
 namespace ML.NET___Breast_Cancer
 {
     class Program
     {
+
+        const string modelPath = "@Model.zip";
+
         static void Main(string[] args)
         {
+            PredictionModel<CancerData, CancerPrediction> model = Train();
+            Evaluate(model);
+        }
+
+        public static PredictionModel<CancerData, CancerPrediction> Train()
+        {
             var pipeline = new LearningPipeline();
-            
-            Console.WriteLine("Hello World!");
+            pipeline.Add(new TextLoader("Cancer-Train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';'));
+            pipeline.Add(new Dictionarizer(("Diagnosis", "Label")));
+            pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
+                "RadiusMean",
+                "TextureMean",
+                "PerimeterMean",
+                "AreaMean",
+                "SmoothnessMean",
+                "CompactnessMean",
+                "ConcavityMean",
+                "ConcavePointsMean",
+                "SymmetryMean",
+                "FractialDimensionMean",
+                "RadiusSe",
+                "TextureSe",
+                "PerimeterSe",
+                "AreaSe",
+                "SmoothnessSe",
+                "CompactnessSe",
+                "ConcavitySe",
+                "ConcavePointsSe",
+                "SymmetrySe",
+                "FractalDimensionSe",
+                "RadiusWorst",
+                "TextureWorst",
+                "PerimeterWorst",
+                "AreaWorst",
+                "SmoothnessWorst",
+                "CompactnessWorst",
+                "ConcavityWorst",
+                "ConcavPointsWorst",
+                "SymmetryWorst",
+                "FractalDimensionWorst"));
+            pipeline.Add(new StochasticDualCoordinateAscentBinaryClassifier());
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+            PredictionModel<CancerData, CancerPrediction> model = pipeline.Train<CancerData, CancerPrediction>();
+
+            model.WriteAsync(modelPath);
+            return model;
+
+        }
+
+        public static void Evaluate(PredictionModel<CancerData, CancerPrediction> model)
+        {
+            var testData = new TextLoader("Cancer-train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';');
+            var evaluator = new BinaryClassificationEvaluator();
+            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
+
+            var accuracy = metrics.Accuracy;
+            Console.WriteLine("The Accuracy of that model is: " + accuracy);
+            Console.ReadLine();
         }
     }
 }
