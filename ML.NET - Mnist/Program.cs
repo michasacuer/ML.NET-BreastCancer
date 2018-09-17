4faa579820ffb502cc01e88c5e60c8596e11beff
Michał Szewczak <michalszewczak2@gmail.com>
2018-09-17 20:12:22 +0200

    MNIST init

---
+++ ML.NET - Mnist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.NET___Mnist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
