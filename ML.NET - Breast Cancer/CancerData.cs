6eb2ef96b2800789b8cc9017ad28f7f09a51c5d2
Michał Szewczak <michasacuer3@gmail.com>
2019-02-11 18:38:28 +0100

    working on ml.net v 0.10

--- ML.NET - Breast Cancer/CancerData.cs
+++ ML.NET - Breast Cancer/CancerData.cs
@@ -1,109 +1,14 @@
-﻿using Microsoft.ML.Runtime.Api;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.ML.Data;
+
 
 namespace ML.NET___Breast_Cancer
 {
     class CancerData
     {
+        [LoadColumn(0, 30), ColumnName("Features")]
+        public float FeatureVector { get; set; }
 
-        [Column(ordinal: "0")]
-        public string Diagnosis;
-
-        [Column(ordinal: "1")]
-        public float RadiusMean;
-
-        [Column(ordinal: "2")]
-        public float TextureMean;
-
-        [Column(ordinal: "3")]
-        public float PerimeterMean;
-
-        [Column(ordinal: "4")]
-        public float AreaMean;
-
-        [Column(ordinal: "5")]
-        public float SmoothnessMean;
-
-        [Column(ordinal: "6")]
-        public float CompactnessMean;
-
-        [Column(ordinal: "7")]
-        public float ConcavityMean;
-
-        [Column(ordinal: "8")]
-        public float ConcavePointsMean;
-
-        [Column(ordinal: "9")]
-        public float SymmetryMean;
-
-        [Column(ordinal: "10")]
-        public float FractialDimensionMean;
-
-        [Column(ordinal: "11")]
-        public float RadiusSe;
-
-        [Column(ordinal: "12")]
-        public float TextureSe;
-
-        [Column(ordinal: "13")]
-        public float PerimeterSe;
-
-        [Column(ordinal: "14")]
-        public float AreaSe;
-
-        [Column(ordinal: "15")]
-        public float SmoothnessSe;
-
-        [Column(ordinal: "16")]
-        public float CompactnessSe;
-
-        [Column(ordinal: "17")]
-        public float ConcavitySe;
-
-        [Column(ordinal: "18")]
-        public float ConcavePointsSe;
-
-        [Column(ordinal: "19")]
-        public float SymmetrySe;
-
-        [Column(ordinal: "20")]
-        public float FractalDimensionSe;
-
-        [Column(ordinal: "21")]
-        public float RadiusWorst;
-
-        [Column(ordinal: "22")]
-        public float TextureWorst;
-
-        [Column(ordinal: "23")]
-        public float PerimeterWorst;
-
-        [Column(ordinal: "24")]
-        public float AreaWorst;
-
-        [Column(ordinal: "25")]
-        public float SmoothnessWorst;
-
-        [Column(ordinal: "26")]
-        public float CompactnessWorst;
-
-        [Column(ordinal: "27")]
-        public float ConcavityWorst;
-
-        [Column(ordinal: "28")] 
-        public float ConcavPointsWorst;
-
-        [Column(ordinal: "29")]
-        public float SymmetryWorst;
-
-        [Column(ordinal: "30")]
-        public float FractalDimensionWorst;
-
-        [Column(ordinal: "31", name: "Label")]
-        public string Label;
-
-
+        [LoadColumn(31)]
+        public float Target { get; set; }
     }
 }
--- ML.NET - Breast Cancer/Program.cs
+++ ML.NET - Breast Cancer/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using Accord.DataSets;
-using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Transforms;
 using Microsoft.ML.Trainers;
-using Microsoft.ML.Models;
+using Microsoft.ML;
 
 namespace ML.NET___Breast_Cancer
 {
@@ -15,64 +13,66 @@ namespace ML.NET___Breast_Cancer
 
         static void Main(string[] args)
         {
-            PredictionModel<CancerData, CancerPrediction> model = Train();
-            Evaluate(model);
+            var mlContext = new MLContext();
+            var trainData = mlContext.Data.ReadFromTextFile<CancerData>("Cancer-Train.csv", hasHeader: true, separatorChar: ';');
+            //PredictionModel<CancerData, CancerPrediction> model = Train();
+            //Evaluate(model);
         }
 
-        public static PredictionModel<CancerData, CancerPrediction> Train()
-        {
-            var pipeline = new LearningPipeline();
-            pipeline.Add(new TextLoader("Cancer-Train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';'));
-            pipeline.Add(new Dictionarizer(("Diagnosis", "Label")));
-            pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
-                "RadiusMean",
-                "TextureMean",
-                "PerimeterMean",
-                "AreaMean",
-                "SmoothnessMean",
-                "CompactnessMean",
-                "ConcavityMean",
-                "ConcavePointsMean",
-                "SymmetryMean",
-                "FractialDimensionMean",
-                "RadiusSe",
-                "TextureSe",
-                "PerimeterSe",
-                "AreaSe",
-                "SmoothnessSe",
-                "CompactnessSe",
-                "ConcavitySe",
-                "ConcavePointsSe",
-                "SymmetrySe",
-                "FractalDimensionSe",
-                "RadiusWorst",
-                "TextureWorst",
-                "PerimeterWorst",
-                "AreaWorst",
-                "SmoothnessWorst",
-                "CompactnessWorst",
-                "ConcavityWorst",
-                "ConcavPointsWorst",
-                "SymmetryWorst",
-                "FractalDimensionWorst"));
-            pipeline.Add(new StochasticDualCoordinateAscentBinaryClassifier());
-            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
-            PredictionModel<CancerData, CancerPrediction> model = pipeline.Train<CancerData, CancerPrediction>();
+        //public static PredictionModel<CancerData, CancerPrediction> Train()
+        //{
+        //    var pipeline = new LearningPipeline();
+        //    pipeline.Add(new TextLoader("Cancer-Train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';'));
+        //    pipeline.Add(new Dictionarizer(("Diagnosis", "Label")));
+        //    pipeline.Add(new ColumnConcatenator(outputColumn: "Features",
+        //        "RadiusMean",
+        //        "TextureMean",
+        //        "PerimeterMean",
+        //        "AreaMean",
+        //        "SmoothnessMean",
+        //        "CompactnessMean",
+        //        "ConcavityMean",
+        //        "ConcavePointsMean",
+        //        "SymmetryMean",
+        //        "FractialDimensionMean",
+        //        "RadiusSe",
+        //        "TextureSe",
+        //        "PerimeterSe",
+        //        "AreaSe",
+        //        "SmoothnessSe",
+        //        "CompactnessSe",
+        //        "ConcavitySe",
+        //        "ConcavePointsSe",
+        //        "SymmetrySe",
+        //        "FractalDimensionSe",
+        //        "RadiusWorst",
+        //        "TextureWorst",
+        //        "PerimeterWorst",
+        //        "AreaWorst",
+        //        "SmoothnessWorst",
+        //        "CompactnessWorst",
+        //        "ConcavityWorst",
+        //        "ConcavPointsWorst",
+        //        "SymmetryWorst",
+        //        "FractalDimensionWorst"));
+        //    pipeline.Add(new StochasticDualCoordinateAscentBinaryClassifier());
+        //    pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+        //    PredictionModel<CancerData, CancerPrediction> model = pipeline.Train<CancerData, CancerPrediction>();
 
-            model.WriteAsync(modelPath);
-            return model;
+        //    model.WriteAsync(modelPath);
+        //    return model;
 
-        }
+        //}
 
-        public static void Evaluate(PredictionModel<CancerData, CancerPrediction> model)
-        {
-            var testData = new TextLoader("Cancer-train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';');
-            var evaluator = new BinaryClassificationEvaluator();
-            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
+        //public static void Evaluate(PredictionModel<CancerData, CancerPrediction> model)
+        //{
+        //    var testData = new TextLoader("Cancer-train.csv").CreateFrom<CancerData>(useHeader: true, separator: ';');
+        //    var evaluator = new BinaryClassificationEvaluator();
+        //    BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
 
-            var accuracy = metrics.Accuracy;
-            Console.WriteLine("The Accuracy of that model is: " + accuracy);
-            Console.ReadLine();
-        }
+        //    var accuracy = metrics.Accuracy;
+        //    Console.WriteLine("The Accuracy of that model is: " + accuracy);
+        //    Console.ReadLine();
+        //}
     }
 }
